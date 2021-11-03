04971b466f44a777bf872fa722fe9f9544849314
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-03 13:31:51 +0100

    change mobile image format to 64bit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,15 @@
 	<!--Main Col-->
 	<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
 	
-
+		@php
+			
+			$image = base64_encode(file_get_contents(public_path('storage').'/'.setting('profile.picture')));
+		@endphp
 		<div class="p-4 md:p-12 text-center lg:text-left">
 			<!-- Image for mobile view-->
-			<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('{{Voyager::image(setting('profile.picture'))}}')"></div>
+			<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" 
+			style="background-image: url('data:image/jpg;base64,{{$image}}')">
+		</div>
 			
 			<h1 class="text-3xl font-bold pt-8 lg:pt-0">Elmarzougui Abdelghafour</h1>
 			<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
@@ -56,10 +61,7 @@
 	<!--Img Col-->
 	<div class="w-full lg:w-2/5">
 		<!-- Big profile image for side bar (desktop) -->
-		@php
-			
-			$image = base64_encode(file_get_contents(public_path('storage').'/'.setting('profile.picture')));
-		@endphp
+
 		<img src="data:image/jpg;base64,{{$image}}" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" oncontextmenu="return false;">
 
 	</div>
